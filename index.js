c9a547949416e9d926d6662578708157aae34332
iperez13 <iperez13@ucol.mx>
2023-11-16 17:17:09 -0600

    second version

--- index.js
+++ index.js
@@ -46,30 +46,77 @@
 
 // function restar(a,b function()){{console.log("hola antho");}}
 
-function suma() {
-    let x = parseFloat(document.getElementById ("numero1").value);
-    let y = parseFloat(document.getElementById ("numero2").value);
-    let resultado = x + y;
-    console.log(resultado);
-}
-function resta() {
-    let x = parseFloat(document.getElementById ("numero1").value);
-    let y = parseFloat(document.getElementById ("numero2").value);
-    let resultado = x - y;
-    console.log(resultado);
-}
-function multiplicacion() {
-    let x = parseFloat(document.getElementById ("numero1").value);
-    let y = parseFloat(document.getElementById ("numero2").value);
-    let resultado = x * y;
-    console.log(resultado);
-}
-function division() {
-    let x = parseFloat(document.getElementById ("numero1").value);
-    let y = parseFloat(document.getElementById ("numero2").value);
-    let resultado = x / y;
-    console.log(resultado);
-}
+// CALULADORA
 
+// function suma() {
+//     let x = parseFloat(document.getElementById ("numero1").value);
+//     let y = parseFloat(document.getElementById ("numero2").value);
+//     let resultado = x + y;
+//     let span = document.querySelector ("#resultado");
+//     span.innerHTML = resultado;
+//     console.log(resultado);
+// }
+
+// function resta() {
+//     let x = parseFloat(document.getElementById ("numero1").value);
+//     let y = parseFloat(document.getElementById ("numero2").value);
+//     let resultado = x - y;
+//     console.log(resultado);
+// }
+// function multiplicacion() {
+//     let x = parseFloat(document.getElementById ("numero1").value);
+//     let y = parseFloat(document.getElementById ("numero2").value);
+//     let resultado = x * y;
+//     console.log(resultado);
+// }
+// function division() {
+//     let x = parseFloat(document.getElementById ("numero1").value);
+//     let y = parseFloat(document.getElementById ("numero2").value);
+//     let resultado = x / y;
+//     console.log(resultado);
+// }
 
 // console.log('la suma es :' + resultado);
+
+
+let boton = document.querySelector(".botonClick");
+
+// EVENTOS DEL MOUSE
+                // evento que se va escuchar : funcion que se va a ejecutar
+boton.addEventListener("click", function(){
+    console.log("dimos click en el boton click");
+    boton.classList.toggle("verde");
+
+})
+
+boton.addEventListener("mouseover", function(){
+    console.log("estamos sobre el boton click");
+
+})
+
+boton.addEventListener("mouseout", function(){
+    console.log("salimos del boton");
+
+})
+
+// EVENTOS DE TECLADO
+
+window.addEventListener('keydown', (e)=>{
+    console.log('pulso una tecla')
+    console.log(String.fromCharCode(e.keyCode))
+})
+
+window.addEventListener('keypress', (e)=>{
+    console.log('presione una tecla')
+    console.log(String.fromCharCode(e.keyCode))
+})
+
+window.addEventListener('keyup', (e)=>{
+    console.log('deje de preionar una tecla')
+    console.log(String.fromCharCode(e.keyCode))
+})
+
+window.addEventListener('load', ()=>{
+    console.log('termine de cargar la pagina');
+});
+
